c86eeb869bc8845aea23ebb58a22c54bab515a7c
William Kim <azamara@me.com>
2015-04-12 13:20:18 +0900

    update log

--- lm35-temperature-sensor/lm35-temperature-sensor.ino
+++ lm35-temperature-sensor/lm35-temperature-sensor.ino
@@ -1,17 +1,15 @@
+#define SERIAL_BAUDRATE 9600
+
 int outputPin = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(SERIAL_BAUDRATE);
 }
 
 void loop() {
   int rawvoltage = analogRead(outputPin);
   float millivolts= (rawvoltage/1024.0) * 5000;
   float celsius= millivolts/10;
-  Serial.print(celsius);
-  Serial.print(" degrees Celsius, ");
-  
-  Serial.print((celsius * 9)/5 + 32);
-  Serial.println(" degrees Fahrenheit");
-  delay(1000);
+  Serial.println(celsius);
+  delay(5000);
 }
