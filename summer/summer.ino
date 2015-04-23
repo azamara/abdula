1b79a964dff3cdff35585aeffb146ed31720d2d7
William Kim <azamara@me.com>
2015-04-23 22:50:59 +0900

    refactor: update DHT22 -> DHT11

--- summer/summer.ino
+++ summer/summer.ino
@@ -3,8 +3,8 @@
 #define DHTPIN 7     // what pin we're connected to
 
 // Uncomment whatever type you're using!
-#define DHTTYPE DHT11   // DHT 11 
-//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT11   // DHT 11 
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 // Connect pin 1 (on the left) of the sensor to +5V
